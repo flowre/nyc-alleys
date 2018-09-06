63b7fe8c8a4e253c22928d235b85ff2db0428be5
flxmdy <flxmdy@gmail.com>
2018-09-06 14:23:59 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var map = L.map('map').setView([40.6971494, -74], 11)
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
+    {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.satellite',
+      accessToken: 'pk.eyJ1IjoiZmx4bWR5IiwiYSI6ImNqbHB4YnRxNjA2a3Izc3F3a2E5dWF2czYifQ.suDzu_w_2m97miGg1M3o3g'
+    }).addTo(map);
+
+  fetch("https://data.cityofnewyork.us/resource/f2xj-tahy.json?rw_type=10&%24limit=20000")
+    .then(res => res.json())
+    .then(json => json.map(x => x.the_geom))
+    .then(geoms => L.geoJSON(geoms).addTo(map))
+    .catch(err => console.error('Error:', err))
+})
